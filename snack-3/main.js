4313da061e4c27606233a3ec13659f42a17e1d87
matteomedori <matteo.medori.97@gmail.com>
2023-11-10 11:53:32 +0100

    feat: snack 3

---
+++ snack-3/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+//inizializzo variabile somma
+let somma = 0;
+
+//ciclo di 10 iterazioni
+for (let i = 0; i < 10; i++) {
+  //salvo numero inserito dall'utente
+  const numeroInserito = Number(prompt("inserisci un numero"));
+
+  //sommo ogni numero alla somma complessiva
+  somma += numeroInserito;
+}
+
+console.log(somma);
