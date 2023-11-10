361ee5f42c1ede1ddc56e83ec5668587dfa8eb7e
matteomedori <matteo.medori.97@gmail.com>
2023-11-10 11:19:33 +0100

    feat: snack 2

---
+++ snack-2/main.js
@@ -0,0 +1,16 @@
+"use strict";
+//L’utente inserisce due parole in successione, con due prompt.
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const parola1 = prompt("Inserisci la prima parola");
+const parola2 = prompt("Inserisci la seconda parola");
+
+if (parola1.length > parola2.length) {
+  console.log(parola2);
+  console.log(parola1);
+} else if (parola1.length < parola2.length) {
+  console.log(parola1);
+  console.log(parola2);
+} else {
+  console.log("Le parole hanno stessa lunghezza");
+}
