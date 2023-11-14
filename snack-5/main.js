07a6fce6533e7bb42926f7f0320c7bdd80d5b360
matteomedori <matteo.medori.97@gmail.com>
2023-11-14 12:35:30 +0100

    feat: commenti

--- snack-5/main.js
+++ snack-5/main.js
@@ -1,11 +1,17 @@
 "use strict";
-/*Fai inserire un numero, che chiameremo N, all’utente.
-Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.*/
+/*
+Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
+*/
+
+//numero inserito dall'utente
 const numero = Number(prompt("Inserisci un numero"));
 
 if (!isNaN(numero)) {
+  //ciclo che esegue tante volte quanto il numero inserito
   for (let i = 0; i < numero; i++) {
     const array = [];
+    //ciclo che esegue 10 volte
     for (let j = 0; j < 10; j++) {
       const numeroRandom = Math.floor(Math.random() * 101);
       array.push(numeroRandom);
